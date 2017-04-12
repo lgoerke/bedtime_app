d0d5bcdefb45bcec52a92e7845a683816520354b
lgoerke <lgoerke@uni-osnabrueck.de>
2017-04-12 17:15:38 +0200

    started with Questionnaire, currently start screen

--- BedtimeApp/app/src/main/java/applab/bedtimeapp/MainDrawerActivity.java
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/MainDrawerActivity.java
@@ -39,38 +39,38 @@ public class MainDrawerActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_activity_main);
 
-        //  Declare a new thread to do a preference check
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                //  Initialize SharedPreferences
-                SharedPreferences getPrefs = PreferenceManager
-                        .getDefaultSharedPreferences(getBaseContext());
-
-                //  Create a new boolean and preference and set it to true
-                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
-
-                //  If the activity has never started before...
-                if (isFirstStart) {
-
-                    //  Launch app intro
-                    Intent i = new Intent(MainDrawerActivity.this, TutorialIntro.class);
-                    startActivity(i);
-
-                    //  Make a new preferences editor
-                    SharedPreferences.Editor e = getPrefs.edit();
-
-                    //  Edit preference to make it false because we don't want this to run again
-                    e.putBoolean("firstStart", false);
-
-                    //  Apply changes
-                    e.apply();
-                }
-            }
-        });
-
-        // Start the thread
-        t.start();
+//        //  Declare a new thread to do a preference check
+//        Thread t = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                //  Initialize SharedPreferences
+//                SharedPreferences getPrefs = PreferenceManager
+//                        .getDefaultSharedPreferences(getBaseContext());
+//
+//                //  Create a new boolean and preference and set it to true
+//                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+//
+//                //  If the activity has never started before...
+//                if (isFirstStart) {
+//
+//                    //  Launch app intro
+//                    Intent i = new Intent(MainDrawerActivity.this, TutorialIntro.class);
+//                    startActivity(i);
+//
+//                    //  Make a new preferences editor
+//                    SharedPreferences.Editor e = getPrefs.edit();
+//
+//                    //  Edit preference to make it false because we don't want this to run again
+//                    e.putBoolean("firstStart", false);
+//
+//                    //  Apply changes
+//                    e.apply();
+//                }
+//            }
+//        });
+//
+//        // Start the thread
+//        t.start();
 
 
 
