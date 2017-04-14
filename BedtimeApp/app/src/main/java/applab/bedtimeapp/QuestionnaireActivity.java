890afb35f1270b0eabc6ddae69c2f83b13d27106
wdwit <willem_de_wit@hotmail.com>
2017-04-14 18:34:29 +0200

    Made database
    Created the databaseHelper class

--- BedtimeApp/app/src/main/java/applab/bedtimeapp/QuestionnaireActivity.java
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/QuestionnaireActivity.java
@@ -1,20 +1,27 @@
 package applab.bedtimeapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 
+import applab.bedtimeapp.utils.DatabaseHelper;
+
 public class QuestionnaireActivity extends AppCompatActivity {
 
     private boolean metBedtime = false;
+    private DatabaseHelper database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questionnaire);
+
+        // get database
+        database = new DatabaseHelper(QuestionnaireActivity.this);
 
     }
 
@@ -29,6 +36,8 @@ public class QuestionnaireActivity extends AppCompatActivity {
                 btn = (Button) findViewById(R.id.btn_no);
                 btn.setPressed(false);
                 metBedtime = true;
+                SQLiteDatabase db_write = database.getWritableDatabase();
+                // do stuff to write question3 information in database
                 break;
             case R.id.btn_no:
                 btn = (Button) findViewById(R.id.btn_no);
@@ -51,6 +60,12 @@ public class QuestionnaireActivity extends AppCompatActivity {
         Intent output = new Intent();
         setResult(RESULT_OK, output);
         finish();
+    }
+
+    @Override
+    protected void onStop() {
+        database.close();
+        super.onStop();
     }
 
 
