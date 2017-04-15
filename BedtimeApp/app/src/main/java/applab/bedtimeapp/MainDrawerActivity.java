d6d98d4fd05e8d579b83c565c6e3eb29d3e0d40c
berberakif <mehmetakifberber@gmail.com>
2017-04-16 01:02:28 +0200

    Calling questionnaire via notification

--- BedtimeApp/app/src/main/java/applab/bedtimeapp/MainDrawerActivity.java
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/MainDrawerActivity.java
@@ -1,8 +1,15 @@
 package applab.bedtimeapp;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,6 +18,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -212,5 +220,49 @@ public class MainDrawerActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.notification_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_5:
+                scheduleNotification(getNotification("5 second delay"), 5000);
+                return true;
+            case R.id.action_10:
+                scheduleNotification(getNotification("10 second delay"), 10000);
+                return true;
+            case R.id.action_30:
+                scheduleNotification(getNotification("30 second delay"), 30000);
+                return true;
+            default:
+                //return true;
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    private void scheduleNotification(Notification notification, int delay) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+    }
+
+    private Notification getNotification(String content) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Scheduled Notification");
+        builder.setContentText(content);
+        builder.setSmallIcon(R.drawable.icon);
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        builder.setSound(uri);
+        return builder.build();
     }
 }
---
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/NotificationPublisher.java
@@ -0,0 +1,39 @@
+package applab.bedtimeapp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+public class NotificationPublisher extends BroadcastReceiver {
+
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+
+    public void onReceive(Context context, Intent intent) {
+
+       /* NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+        notificationManager.notify(id, notification);*/
+
+
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = intent.getParcelableExtra(NOTIFICATION);
+        int id = intent.getIntExtra(NOTIFICATION_ID, 0);
+       // notificationManager.notify(id, notification);
+
+        notificationManager.cancel(intent.getExtras().getInt("id"));
+        Toast.makeText(context, "HEY HEY HEY", Toast.LENGTH_LONG).show();
+        intent= new Intent(context, QuestionnaireActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //intent.putExtra("attachMedia",true);
+        context.startActivity(intent);
+    }
+}
