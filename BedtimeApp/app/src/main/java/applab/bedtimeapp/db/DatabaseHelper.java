dd0cfbe245d24b64a722044cb19175d84d83cac3
wdwit <willem_de_wit@hotmail.com>
2017-05-16 09:35:29 +0200

    Update DB to test for JSON export

--- BedtimeApp/app/src/main/java/applab/bedtimeapp/db/DatabaseHelper.java
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/db/DatabaseHelper.java
@@ -1,11 +1,19 @@
 package applab.bedtimeapp.db;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
+
+    //
+    public String databasePath = "";
 
     // Database version
     private static final int DATABASE_VERSION = 1;
@@ -91,6 +99,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        databasePath = context.getDatabasePath("Bedtime.db").getPath();
     }
 
     @Override
@@ -107,5 +116,57 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_REASON);
 
         onCreate(db);
+    }
+
+    public JSONArray getResults()
+    {
+
+
+        // TODO other 2 tables and test
+        String myTable = TABLE_FEEDBACK;//Set name of your table
+
+        //String dbPath = context.getDatabasePath("Bedtime.db");
+
+        SQLiteDatabase bedtimeDB = SQLiteDatabase.openDatabase(databasePath, null, SQLiteDatabase.OPEN_READONLY);
+
+        String searchQuery = "SELECT  * FROM " + myTable;
+        Cursor cursor = bedtimeDB.rawQuery(searchQuery, null );
+
+        JSONArray resultSet = new JSONArray();
+
+        cursor.moveToFirst();
+        while (cursor.isAfterLast() == false) {
+
+            int totalColumn = cursor.getColumnCount();
+            JSONObject rowObject = new JSONObject();
+
+            for( int i=0 ;  i< totalColumn ; i++ )
+            {
+                if( cursor.getColumnName(i) != null )
+                {
+                    try
+                    {
+                        if( cursor.getString(i) != null )
+                        {
+                            Log.d("TAG_NAME", cursor.getString(i) );
+                            rowObject.put(cursor.getColumnName(i) ,  cursor.getString(i) );
+                        }
+                        else
+                        {
+                            rowObject.put( cursor.getColumnName(i) ,  "" );
+                        }
+                    }
+                    catch( Exception e )
+                    {
+                        Log.d("TAG_NAME", e.getMessage()  );
+                    }
+                }
+            }
+            resultSet.put(rowObject);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        Log.d("TAG_NAME", resultSet.toString() );
+        return resultSet;
     }
 }
