6d2a22183aa51da9ac22dee8293f8a1e437fdf1c
lgoerke <lgoerke@uni-osnabrueck.de>
2017-05-23 18:25:44 +0200

    further stuff coach

--- BedtimeApp/app/src/main/java/applab/bedtimeapp/CoachActivity.java
+++ BedtimeApp/app/src/main/java/applab/bedtimeapp/CoachActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -29,6 +31,8 @@ public class CoachActivity extends AppCompatActivity {
     private int height;
     private FeedbackOperations feedbackData;
     private AlarmOperations alarmData;
+    private final int DATES = 1;
+    private final int RATING = 2;
 
     private Date getDateFromString(String str) {
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -38,7 +42,6 @@ public class CoachActivity extends AppCompatActivity {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        Log.e("yo",date.toString());
         return date;
     }
 
@@ -50,27 +53,26 @@ public class CoachActivity extends AppCompatActivity {
         feedbackData = new FeedbackOperations(this);
         feedbackData.open();
 
-        List<Integer> restednesses = new ArrayList<Integer>();
-        List<Integer> busynesses = new ArrayList<Integer>();
-        List<Integer> moods = new ArrayList<Integer>();
-        List<Integer> concentrations = new ArrayList<Integer>();
-        List<Date> datesF = new ArrayList<Date>();
+        ArrayList<Integer> restednesses = new ArrayList<Integer>();
+        ArrayList<Integer> busynesses = new ArrayList<Integer>();
+        ArrayList<Integer> moods = new ArrayList<Integer>();
+        ArrayList<Integer> concentrations = new ArrayList<Integer>();
+        ArrayList<Date> datesF = new ArrayList<Date>();
 
-        List<Feedback> feedbacks = feedbackData.getAllFeedbacks(-1);
+        ArrayList<Feedback> feedbacks = new ArrayList<>(feedbackData.getAllFeedbacks(-1));
         feedbackData.close();
 
         for (int i = 0; i < feedbacks.size(); i++) {
             datesF.add(getDateFromString(feedbacks.get(i).getDate()));
             restednesses.add(feedbacks.get(i).getQuestionRested());
-            busynesses.add(feedbacks.get(i).getQuestionRested());
-            moods.add(feedbacks.get(i).getQuestionRested());
-            concentrations.add(feedbacks.get(i).getQuestionRested());
+            busynesses.add(feedbacks.get(i).getQuestionBusy());
+            moods.add(feedbacks.get(i).getQuestionMood());
+            concentrations.add(feedbacks.get(i).getQuestionConcentration());
         }
 
         alarmData = new AlarmOperations(this);
         alarmData.open();
 
-        List<Integer> sleeprates = new ArrayList<Integer>();
 
         //  Initialize SharedPreferences
         SharedPreferences getPrefs = PreferenceManager
@@ -79,8 +81,9 @@ public class CoachActivity extends AppCompatActivity {
         //  Create a new boolean and preference and set it to true
         int userID = getPrefs.getInt("userID", 0);
 
-        List<Alarm> alarms = alarmData.getAllAlarms(userID);
-        List<Date> datesA = new ArrayList<Date>();
+        ArrayList<Alarm> alarms = new ArrayList<>(alarmData.getAllAlarms(userID));
+        ArrayList<Date> datesA = new ArrayList<Date>();
+        ArrayList<Integer> sleeprates = new ArrayList<Integer>();
         alarmData.close();
 
         for (int i = 0; i < alarms.size(); i++) {
@@ -88,50 +91,23 @@ public class CoachActivity extends AppCompatActivity {
             sleeprates.add(alarms.get(i).getSleepRate());
         }
 
-        Date d = new Date(2017,05,19);
-        datesF.add(d);
-        datesF.add(d);
-        busynesses.add(null);
-        busynesses.add(2);
+//        ArrayList<Date> datesACleaned = (ArrayList<Date>) cleanData(datesA, sleeprates, DATES);
+//        ArrayList<Integer> sleepratesCleaned = (ArrayList<Integer>) cleanData(datesA, sleeprates, RATING);
+
+        ArrayList<Date> datesFCleaned = (ArrayList<Date>) cleanData(datesF, restednesses, DATES);
+        ArrayList<Integer> restednessCleaned = (ArrayList<Integer>) cleanData(datesF, restednesses, RATING);
+        ArrayList<Integer> busynessCleaned = (ArrayList<Integer>) cleanData(datesF, busynesses, RATING);
+        ArrayList<Integer> moodsCleaned = (ArrayList<Integer>) cleanData(datesF, moods, RATING);
+        ArrayList<Integer> concentrationCleaned = (ArrayList<Integer>) cleanData(datesF, concentrations, RATING);
 
 
-        Log.e("dates",datesF.toString());
-        Log.e("busyness",busynesses.toString());
+        Log.e("Busy", busynessCleaned.toString());
+        Log.e("Rested",restednessCleaned.toString());
 
-        ArrayList<Date> datesFcleaned = new ArrayList<Date>();
-        ArrayList<Integer> busyCleaned = new ArrayList<Integer>();
-        ArrayList<Integer> stash = new ArrayList<Integer>();
-        Date currentDate = datesF.get(0);
-        for (int i = 0; i < datesF.size(); i++) {
-            Log.e("ith data",datesF.get(i).toString());
-            Calendar c1 = Calendar.getInstance();
-            Calendar c2 = Calendar.getInstance();
-
-            c1.setTime(datesF.get(i));
-            c2.setTime(currentDate);
-
-            int yearDiff = c1.get(Calendar.YEAR) - c2.get(Calendar.YEAR);
-            int monthDiff = c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
-            int dayDiff = c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH);
-
-            if (!(yearDiff == 0 && monthDiff == 0 && dayDiff == 0)){
-                datesFcleaned.add(datesF.get(i));
-                Log.e("stash",stash.toString());
-                stash.removeAll(Collections.singleton(null));
-                Log.e("stash",stash.toString());
-                busyCleaned.add(stash.get(0));
-                stash = new ArrayList<Integer>();
-                stash.add(busynesses.get(i));
-                currentDate = datesF.get(i);
-            } else {
-                stash.add(busynesses.get(i));
-            }
-
-            Log.e("dates",datesFcleaned.toString());
-            Log.e("busy",busyCleaned.toString());
-
-        }
-
+        SimpleRegression regr = getRegression(restednessCleaned,busynessCleaned);
+        Log.e("Slope",Double.toString(regr.getSlope()));
+        Log.e("Significance",Double.toString(regr.getSignificance()));
+        Log.e("MSE",Double.toString(regr.getMeanSquareError()));
 
 //        RelativeLayout rl = (RelativeLayout) findViewById(R.id.coach_view);
 //        final ImageView iv = (ImageView) rl.findViewById(R.id.owl);
@@ -164,6 +140,59 @@ public class CoachActivity extends AppCompatActivity {
 //                });
 
 
+    }
+
+    public SimpleRegression getRegression(ArrayList<Integer> firstList, ArrayList<Integer> secondList) {
+
+        SimpleRegression regression = new SimpleRegression();
+
+        for (int i = 0; i < firstList.size(); i++){
+            regression.addData(((double) firstList.get(i)), ((double) secondList.get(i)));
+        }
+
+        return regression;
+    }
+
+    public ArrayList cleanData(ArrayList<Date> dates, ArrayList<Integer> rating, int mode) {
+
+        ArrayList<Date> datesCleaned = new ArrayList<Date>();
+        ArrayList<Integer> ratingCleaned = new ArrayList<Integer>();
+        ArrayList<Integer> stash = new ArrayList<Integer>();
+        Date currentDate = dates.get(0);
+        for (int i = 0; i < dates.size(); i++) {
+            Calendar c1 = Calendar.getInstance();
+            Calendar c2 = Calendar.getInstance();
+
+            c1.setTime(dates.get(i));
+            c2.setTime(currentDate);
+
+            int yearDiff = c1.get(Calendar.YEAR) - c2.get(Calendar.YEAR);
+            int monthDiff = c1.get(Calendar.MONTH) - c2.get(Calendar.MONTH);
+            int dayDiff = c1.get(Calendar.DAY_OF_MONTH) - c2.get(Calendar.DAY_OF_MONTH);
+
+            if (!(yearDiff == 0 && monthDiff == 0 && dayDiff == 0)) {
+                datesCleaned.add(dates.get(i - 1));
+                stash.removeAll(Collections.singleton(null));
+                ratingCleaned.add(stash.get(0));
+                stash = new ArrayList<Integer>();
+                stash.add(rating.get(i));
+                currentDate = dates.get(i);
+            } else {
+                stash.add(rating.get(i));
+            }
+        }
+
+        datesCleaned.add(dates.get(dates.size() - 1));
+        stash.removeAll(Collections.singleton(null));
+        ratingCleaned.add(stash.get(0));
+
+        if (mode == DATES) {
+            return dates;
+        } else if (mode == RATING) {
+            return rating;
+        }
+
+        return null;
 
     }
 
